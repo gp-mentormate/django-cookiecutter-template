fc7c65c280a9c465e7740a1c55eebb19dfed68db
georgi.penev <georgi.penev@mentormate.com>
2023-05-23 16:55:49 +0300

    update post generation project's script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,16 +7,20 @@ def create_virtual_environment():
     # Create a virtual environment using the current Python version
     subprocess.run(["python", "-m", "venv", "env"])
 
+    # Upgrade pip version
+    subprocess.run(["./env/bin/python3", "-m", "pip", "install", "--upgrade", "pip"])
+
 
 def install_requirements():
     # Determine the requirements file based on the environment parameter
     requirements_file = f'requirements/local.txt'
 
     # Execute the pip command to install the requirements
+    # TODO: set correct windows venv python path
     if platform.system() == 'Windows':
-        subprocess.run(["pip", "install", "-r", requirements_file], shell=True)
+        subprocess.run(["./env/bin/python3", "-m", "pip", "install", "-r", requirements_file], shell=True)
     else:
-        subprocess.run(["pip", "install", "-r", requirements_file])
+        subprocess.run(["./env/bin/python3", "-m", "pip", "install", "-r", requirements_file])
 
 
 # Call the initial setup functions
