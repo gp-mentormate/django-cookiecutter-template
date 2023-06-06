95546c48903adf7698c86424da01e520e5074d10
Georgi Penev <georgi.penev@mentormate.com>
2023-06-06 19:48:31 +0300

    tests refactoring

---
+++ {{cookiecutter.project_name}}/apps/auth/api/v1/tests/test_registration.py
@@ -0,0 +1,52 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from faker import Faker
+
+
+class UserRegistrationTest(APITestCase):
+    def setUp(self):
+        self.fake = Faker()
+        self.register_url = reverse("customuser-list")  # URL for user registration
+
+    def test_user_registration(self):
+        # Arrange
+        # Prepare data for registration
+        user_data = {
+            "username": self.fake.user_name(),
+            "email": self.fake.email(),
+            "password": self.fake.password(),
+        }
+
+        # Act
+        # Make a POST request to register a new user
+        response = self.client.post(self.register_url, user_data)
+
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(get_user_model().objects.count(), 1)
+
+        self.assertEqual(response.data["username"], user_data["username"])
+        self.assertEqual(response.data["email"], user_data["email"])
+        self.assertTrue("password" not in response.data)  # Ensure password is not returned
+
+    def test_user_registration_existing_username(self):
+        # Arrange
+        user_data = {
+            "username": self.fake.user_name(),
+            "email": self.fake.email(),
+            "password": self.fake.password(),
+        }
+
+        # Create a user with the same username
+        get_user_model().objects.create_user(**user_data)
+
+        # Act
+        response = self.client.post(self.register_url, user_data)
+
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertIn("username", response.data)
+        self.assertIn("A user with that username already exists.", response.data["username"])
