6fa7b8541b4c42d17ec64fe92b40c14fe2e0ea37
Georgi Penev <georgi.penev@mentormate.com>
2023-06-07 15:02:20 +0300

    remove unused imports

--- {{cookiecutter.project_name}}/apps/todo/api/v1/views.py
+++ {{cookiecutter.project_name}}/apps/todo/api/v1/views.py
@@ -5,17 +5,12 @@
 import logging
 from typing import Any
 
-from email_from_template import send_mail
 from rest_framework import permissions, status
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from apps.todo.api.v1.serializers import TodoListSerializer
 from apps.todo.api.v1.services import get_todo_lists
 from apps.todo.models import TodoList
-from config.settings.base import ADMINS
 
 
 class TodoListModelViewSet(ModelViewSet):
